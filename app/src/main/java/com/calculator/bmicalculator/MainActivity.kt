db492e7e6bb2edd30fcc53b8e0a31a26fc21f95c
chainconsultsdev <chainprojects@chainconsults.com>
2021-08-21 23:38:43 +0100

    Initial Commit

--- app/src/main/java/com/calculator/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/calculator/bmicalculator/MainActivity.kt
@@ -9,13 +9,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-
-
         btn.setOnClickListener {
             val h = (height.text).toString().toFloat() /100
             val w = weight.text.toString().toFloat()
             val res = w/(h*h)
-            res.text = "%.2f".format(res)
+            result.text = "%.2f".format(res)
         }
     }
 }
